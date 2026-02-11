28137a6bd737a13d101454fbd5467f8e3c1b601a
Nanthakumar <nnantha971@gmail.com>
2026-02-11 16:12:40 +0530

    the feautured intruments box were added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,39 +25,69 @@ function Home() {
     <div className="stats">
       <div className="stat-card">
         <ul>
-            <h3>1.Inter-institution collaboration count</h3>
-            <li>✔ 1000+ Instruments</li>
-            <li>✔ 300+ Institutions</li>
-            <li>✔ Pan-India Access</li>
+            <h3>Inter-institution collaboration count</h3>
+            <li><span className='tick'>✔</span> 1000+ Instruments</li>
+            <li><span className='tick'>✔</span> 300+ Institutions</li>
+            <li><span className='tick'>✔</span> Pan-India Access</li>
         </ul>
         </div>
 
         <div className="stat-card">
         <ul>
-            <h3>2.Total Registered Equipment</h3>
-            <li>✔ 12,480+ Instruments Listed</li>
-            <li>✔ Across universities, research labs</li>
-            <li>✔ government institutes</li>
+            <h3>Total Registered Equipment</h3>
+            <li><span className='tick'>✔</span> 12,480+ Instruments Listed</li>
+            <li><span className='tick'>✔</span> Across universities, research labs</li>
+            <li><span className='tick'>✔</span> government institutes</li>
         </ul>
         </div>
 
         <div className="stat-card">
-        <ul> <h3>3.Active Research Institutions</h3>
-            <li>✔ 1,230+ Institutions Onboarded</li>
-            <li>✔ Including IITs, NITs, central universities</li>
-            <li>✔ private R&D labs</li>
+        <ul> <h3>Active Research Institutions</h3>
+            <li><span className='tick'>✔</span> 1,230+ Institutions Onboarded</li>
+            <li><span className='tick'>✔</span> Including IITs, NITs, central universities</li>
+            <li><span className='tick'>✔</span> private R&D labs</li>
         </ul>
         </div>
 
         <div className="stat-card">
-        <ul> <h3>4. Successful Equipment Bookings</h3>
-            <li>✔ 58,900+ Reservations Processed</li>
-            <li>✔ Researchers accessing shared infrastructure nationwide.</li>
-            <li>✔ Pan-India Access</li>
+        <ul> <h3>Successful Equipment Bookings</h3>
+            <li><span className='tick'>✔</span> 58,900+ Reservations Processed</li>
+            <li><span className='tick'>✔</span> Researchers accessing shared infrastructure nationwide.</li>
+            <li><span className='tick'>✔</span> Pan-India Access</li>
         </ul>
         </div>
 
         
+    </div>
+    <div className="featured-instruments">
+        <div className="instrument">
+            <ul>
+            <li>Name: PCR Machine </li>
+            <li>Location: IIT Madras</li>
+            </ul>
+        </div>
+
+        <div className="instrument">
+            <ul>
+            <li>Name: SEM </li>
+            <li>Location: IISc</li>
+            </ul>
+        </div>
+
+        <div className="instrument">
+            <ul>
+            <li>Name: NGS Sequencer </li>
+            <li>Location: NIT Trichy</li>
+            </ul>
+        </div>
+
+
+        <div className="instrument">
+            <ul>
+            <li>Name: Mass Spectrometer</li>
+            <li>Location: Pune</li>
+            </ul>
+        </div>
     </div>
   </div>
 
