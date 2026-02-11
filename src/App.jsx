d2f2d516d0803ab41ad467ddb93782177093f957
Nanthakumar <nnantha971@gmail.com>
2026-02-11 21:31:43 +0530

    notfound page css page created and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home"
 import About from "./About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
      {/* <Home/> */}
-     <About/>
+     {/* <About/> */}
+     <NotFound/>
     </>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./NotFound.css";
+
+function NotFound() {
+  return (
+    <div className="notfound-container">
+      <div className="notfound-content">
+        <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg" alt="not found" />
+
+        <a href="/" className="home-btn">Go Home</a>
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default NotFound;
