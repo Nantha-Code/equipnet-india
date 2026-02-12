8e57dd5476ffc2ad9619afaf9e3d441f916bd3e6
Nanthakumar <nnantha971@gmail.com>
2026-02-12 12:53:19 +0530

    the login page added and the mui packages all installed  and home page nav were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Home from "./pages/Home"
 import About from "./About";
 import NotFound from "./pages/NotFound";
+// import LoginPage from "./pages/LoginPage";
+// import { BrowserRouter } from "react-router";
 
 function App() {
   return (
     <>
-     {/* <Home/> */}
+    {/* <LoginPage/> */}
+     <Home/>
      {/* <About/> */}
-     <NotFound/>
+     {/* <NotFound/> */}
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,16 @@ import About from '../About';
 function Home() {
   return (
 <>
+<header className='top-link'>
+    <nav className='link'>
+        <img src="https://i.pinimg.com/736x/3b/f3/09/3bf309769cc8c9af9fcfa239e90ccbbf.jpg" alt="logo" />
+        <ul>
+            <li> <a href="">Equipment</a></li>
+            <li><a href="">Register</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </nav>
+</header>
 <div className="hero">
   <div className="hero-content">
     
@@ -105,7 +115,6 @@ function Home() {
 
  </div>
   <Footer/> 
-  <About/>
 </> 
   );
 }
