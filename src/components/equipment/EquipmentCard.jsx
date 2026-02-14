60dfba7377135daa1cce995722be4c021f566125
Nanthakumar <nnantha971@gmail.com>
2026-02-14 13:54:54 +0530

    the equipmentcard was created and ui was added

---
+++ src/components/equipment/EquipmentCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function EquipmentCard() {
+  return (
+    <>
+    <div className="equipment-card">
+    <h2>name: IIT Madras, Chennai</h2>
+    <h2>department:Microbiology</h2>
+    <a href="">View Details</a>
+    </div>
+    </>
+  );
+}
+
+export default EquipmentCard;
