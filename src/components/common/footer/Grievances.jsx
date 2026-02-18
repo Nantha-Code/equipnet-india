06e0bb617c46f86d2a01e4c2f7867cceadb13a28
Nanthakumar <nnantha971@gmail.com>
2026-02-18 17:45:07 +0530

    the grievance page content was added

---
+++ src/components/common/footer/Grievances.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Grievances() {
+  return (
+    <>
+    <div className="grievance-main-container">
+<h1>📄 Grievances Page <span className='tittle-span'>(User Complaints & Support)</span></h1>
+<h2>📌 Grievances Redressal Policy</h2>
+<p>
+    Equipment India Platform is committed to providing transparent and reliable access to research equipment. If you face any issues, we encourage you to raise a grievance so we can resolve it quickly.
+
+</p>
+
+<h2>🧾 What You Can Complain About</h2>
+<ul>
+    <li>Equipment booking issues</li>
+    <li>Incorrect equipment information</li>
+    <li>Access denial or delays</li>
+    <li>Technical platform errors</li>
+    <li>Misuse of platform or policy violations</li>
+</ul>
+
+<h2>📮 How to Submit a Grievance</h2>
+<ul>
+    <li>You can submit a grievance through:</li>
+    <li>The Grievance Form on the platform</li>
+    <li>Email: support@equipmentindia.com</li>
+    <li>Contact Dashboard → Support Section</li>
+</ul>
+<h2>⏱️ Resolution Timeline</h2>
+
+<ul>
+    <li>Initial acknowledgment: Within 48 hours</li>
+    <li>Final resolution: Within 7–15 working days</li>
+</ul>
+<h2>👤 Grievance Officer</h2>
+
+
+Name: Platform Support Team
+Email: grievances@equipmentindia.com
+    </div>
+    </>
+  );
+}
+
+export default Grievances;
