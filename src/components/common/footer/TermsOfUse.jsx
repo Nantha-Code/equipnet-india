e20e7cca0d319c303360eebfa1c36c9cd092b92e
Nanthakumar <nnantha971@gmail.com>
2026-02-22 14:43:41 +0530

    the class name added for terms of use page

--- src/components/common/footer/TermsOfUse.jsx
+++ src/components/common/footer/TermsOfUse.jsx
@@ -3,46 +3,53 @@ import React from 'react'
 function TermsOfUse() {
   return (
     <>
+    
     <div className="terms-main-container">
-        <h1> ⚖️ Terms of Use Page</h1>
-        <h2>📌 Terms of Use</h2>
+        <h2 className='tittle-h2'>Terms of Use</h2>
 
-<p>By using Equipment India, you agree to the following terms.</p>
-
-<h3>👤 User Responsibilities</h3>
-<ul>
-    <li>Provide accurate and truthful information </li>
+<div className="tittle-1">
+<p className='tittle-para'>By using Equipment India, you agree to the following terms.</p>
+<h3 className='tittle-h3'>👤 User Responsibilities</h3>
+<ul className='tittle-ul'>
+    <li className='tittle-ul-list'>Provide accurate and truthful information </li>
     
-    <li>Use equipment for academic or approved research purposes</li>
+    <li className='tittle-ul-list'>Use equipment for academic or approved research purposes</li>
     
-    <li>Do not misuse, damage, or illegally access facilities</li>
+    <li className='tittle-ul-list'>Do not misuse, damage, or illegally access facilities</li>
 
-    <li>Respect institution policies</li>
+    <li className='tittle-ul-list'>Respect institution policies</li>
 </ul>
+</div>
 
-
-<h3>🏛️ Institution Responsibilities</h3>
-<ul>
-    <li>Provide correct equipment details</li>
-    <li>Manage booking approvals</li>
-    <li>Ensure equipment safety and availability</li>
+<div className="tittle-2">
+<h3 className='tittle-h3'> Institution Responsibilities</h3>
+<ul className='tittle-ul'>
+    <li className='tittle-ul-list'>Provide correct equipment details</li>
+    <li className='tittle-ul-list'>Manage booking approvals</li>
+    <li className='tittle-ul-list'>Ensure equipment safety and availability</li>
 </ul>
+</div>
 
+<div className="tittle-3">
 
-<h3>🚫 Prohibited Activities</h3>
-<ul>
-    <li>Fake booking requests</li>
-    <li>Data scraping or hacking attempts</li>
-    <li>Commercial misuse without permission</li>
-    <li>Violating institutional or government rules</li>
+<h3 className='tittle-h3'>Prohibited Activities</h3>
+<ul className='tittle-ul'>
+    <li className='tittle-ul-list'>Fake booking requests</li>
+    <li className='tittle-ul-list'>Data scraping or hacking attempts</li>
+    <li className='tittle-ul-list'>Commercial misuse without permission</li>
+    <li className='tittle-ul-list'>Violating institutional or government rules</li>
 </ul>
+</div>
 
-<h3>⚠️ Disclaimer</h3>
+<div className="tittle-4">
 
-<p>
+<h3 className='tittle-h3'>Disclaimer</h3>
+
+<p className='tittle-para'>
     Equipment India is a discovery and request platform. Actual equipment access depends on institution approval. We are not responsible for equipment downtime or research outcomes.
 
 </p>
+</div>
     </div>
     </>
   );
