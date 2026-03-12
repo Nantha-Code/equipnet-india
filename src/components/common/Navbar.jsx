0e9c3baa52e9e744b155e5b4f2f4667fed81d1bf
Nanthakumar <nnantha971@gmail.com>
2026-03-12 13:41:16 +0530

    the  navbar style changed in mobile design and media query was changed

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router';
@@ -6,27 +6,38 @@ import { useNavigate } from 'react-router';
 function Navbar() {
 
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-        <header>
-          <div className="logo-container" onClick={() =>navigate("/")}>
-            <img className="logo" onClick={() =>navigate("/")} src="public/project-logo.png" alt="logo"/>
-          </div>
-            <ul className='nav-link-lists'>
-              <li><Link to="/" className='home-tag'>Home</Link></li>
-              <li><Link to="/equipmentlist" className='equipment-tag'>Equipment</Link></li>
-              <li><Link to="/register" className='register-tag'>Register</Link></li>
-              <li><Link to="/login" className='login-tag'>Login</Link></li>
-              <li>
-                <a href="#about" onClick={(e) => {
-                  e.preventDefault();
-                  document.getElementById("about")?.scrollIntoView({ behavior: "smooth" });
-                }}>About
-                </a>
-              </li> 
-            </ul>
-        </header>
-    </>
+    <header>
+      <div className="logo-container" onClick={() => navigate("/")}>
+        <img className="logo" src="/project-logo.png" alt="logo"/>
+      </div>
+
+      {/* Hamburger Button */}
+      <div 
+        className="hamburger" 
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
+      <ul className={`nav-link-lists ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" className='home-tag'>Home</Link></li>
+        <li><Link to="/equipmentlist" className='equipment-tag'>Equipment</Link></li>
+        <li><Link to="/register" className='register-tag'>Register</Link></li>
+        <li><Link to="/login" className='login-tag'>Login</Link></li>
+        <li>
+          <a href="#about" onClick={(e) => {
+            e.preventDefault();
+            document.getElementById("about")?.scrollIntoView({ behavior: "smooth" });
+          }}>
+            About
+          </a>
+        </li>
+      </ul>
+
+    </header>
   );
 }
 
